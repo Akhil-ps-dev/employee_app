729bb4e9c7f243f82cf9b6731fe6cfdc5dd1a14c
Akhil-ps-dev <psakhil211@gmail.com>
2025-09-16 15:07:50 +0530

    icon added

--- lib/route.dart
+++ lib/route.dart
@@ -1,19 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
 
 class NavigationHelper {
-
-// push to new screen
+  // push to new screen
   static void push(BuildContext context, Widget routeName) {
     Navigator.push(context, MaterialPageRoute(builder: (context) => routeName));
   }
 
-
-
-//replacement of current screen
+  //replacement of current screen
   static void pushReplacement(BuildContext context, Widget routeName) {
     Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => routeName));
+      context,
+      MaterialPageRoute(builder: (context) => routeName),
+    );
   }
 }
